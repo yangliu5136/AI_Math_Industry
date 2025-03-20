47fbecede4cbc30a76df97c6ba92102033b0b290
yangliu <13552165136@163.com>
2025-03-20 15:30:13 +0800

    增加删除redis缓存文件

---
+++ deleteRedisData.py
@@ -0,0 +1,13 @@
+import redis
+# 清除redis中所有缓存
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_client.delete('question1:supply')
+redis_client.delete('question2:supply')
+redis_client.delete('question1:wuliu')
+redis_client.delete('question2:wuliu')
+redis_client.delete('question1:jiagong')
+redis_client.delete('question2:jiagong')
+redis_client.delete('question1:jinrong')
+redis_client.delete('question2:jinrong')
+redis_client.delete('question1:jishu')
+redis_client.delete('question2:jishu')
